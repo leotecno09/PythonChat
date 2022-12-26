dbf8c927be843a877d4173e5969ad1957a5330af
leotecno09 <107548514+leotecno09@users.noreply.github.com>
2022-12-26 11:10:01 +0100

    Update run.cmd

--- run.cmd
+++ run.cmd
@@ -1,6 +1,6 @@
 @echo off
 title Pychat 1.2
-echo Please make sure you installed Python and Pip!
+echo Please make sure you installed Python3 and Pip3!
 echo Installing requirements.txt
 pip3 install -r requirements.txt
 echo Done!
